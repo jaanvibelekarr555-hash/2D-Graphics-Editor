5f64560a8d4ceec9e574fcf65e20ae92ebec2d64
Janhavi Belekar <jaanvibelekarr555@gmail.com>
2026-06-05 09:56:29 +0530

    Added menu system

--- main.c
+++ main.c
@@ -3,9 +3,40 @@
 
 int main()
 {
+    int choice;
+
     initializeCanvas();
 
-    displayCanvas();
+    while(1)
+    {
+        printf("\n===== 2D Graphics Editor =====\n");
+        printf("1. Display Canvas\n");
+        printf("2. Exit\n");
+        printf("Enter choice: ");
+
+        if(scanf("%d", &choice) != 1)
+        {
+            printf("Please enter a valid number!\n");
+
+            while(getchar() != '\n');
+
+            continue;
+        }
+
+        switch(choice)
+        {
+            case 1:
+                displayCanvas();
+                break;
+
+            case 2:
+                printf("Exiting program...\n");
+                return 0;
+
+            default:
+                printf("Invalid Choice!\n");
+        }
+    }
 
     return 0;
 }
